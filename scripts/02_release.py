a2f2e1a37bf0937f99da79ee7bdca2c9c31c1673
Jayanta <jayantadn@proton.me>
2026-06-21 16:45:15 +0530

    release 12.0.0

--- scripts/02_release.py
+++ scripts/02_release.py
@@ -482,13 +482,21 @@ def release(app):
     run_command("flutter clean")
     run_command("flutter pub get")
 
-    # check if build related files are present
+    ############################
+    # build related file paths #
+    ############################
+    cred_file = f"{rootdir}/garuda-1ba07-firebase-adminsdk-fbsvc-c07e3d6e0a.json"
+    key_file = "android/key.properties"
+    firebase_options_file = "lib/firebase_options.dart"
+    google_services_file = "android/app/google-services.json"
+
     files = [
-        f"{rootdir}/garuda-1ba07-firebase-adminsdk-fbsvc-c07e3d6e0a.json",
-        "android/key.properties",
-        "lib/firebase_options.dart",
-        f"android/app/google-services.json",
+        cred_file,
+        key_file,
+        firebase_options_file,
+        google_services_file,
     ]
+
     missing_files = [f for f in files if not os.path.isfile(f)]
     if missing_files:
         print("ERROR: The following required files were not found:")
@@ -496,6 +504,9 @@ def release(app):
             print(f"  - {f}")
         sys.exit(1)
 
+    ############################
+    # hosting deployment       #
+    ############################
     if target in ["web", "both"]:
         print("building for web")
         run_command("flutter build web")
@@ -505,7 +516,9 @@ def release(app):
         run_command(deploy_command, retries=2, retry_delay=10)
 
     if reltype == "release":
-        # function deployment
+        ####################################
+        # function deployment              #
+        ####################################
         if has_firebase_functions(app):
             print("deploying firebase functions")
             run_command("firebase deploy --only functions", retries=2, retry_delay=10)
@@ -514,6 +527,9 @@ def release(app):
                 f"Skipping Firebase functions deployment - no functions configured for {app}"
             )
 
+        ####################################
+        # apk deployment                   #
+        ####################################
         if target in ["apk", "both"]:
             # android build
             print("building for android")
